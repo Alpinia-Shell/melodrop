a9ad62f53a1bf42081e205fe237af8cc940e2678
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-05-27 11:43:11 +0000

    Add:画像のメソッド追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ApplicationRecord
 
   GUEST_USER_EMAIL = "guest@example.com"
 
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image_square.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image_square.jpg', content_type: 'image/jpg')
+    end  
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
   def self.guest_sign_in
     find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
       user.password = SecureRandom.urlsafe_base64

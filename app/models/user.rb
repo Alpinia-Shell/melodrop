3ae341faadfdb44d4c934421c1ff49684142f9cb
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-05-28 02:01:34 +0000

    Add:ユーザーと投稿のアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, length: { minimum: 2 , maximum: 20 }
   has_one_attached :profile_image
+  has_many :posts, dependent: :destroy
 
   GUEST_USER_EMAIL = "guest@example.com"
 

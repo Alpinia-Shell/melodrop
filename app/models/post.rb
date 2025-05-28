920caeb97d2a7e45e13b051f34196d9cae9cd636
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-05-28 03:02:32 +0000

    Add:postモデルバリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  validates :title,  length: { minimum: 2 , maximum: 20 }
+  validates :body, length: { maximum: 200 }
 end

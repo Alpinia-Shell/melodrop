667817952ab4aadea37cf9cd045bf8925c47520c
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-05-28 01:58:44 +0000

    Add:新規投稿機能

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,4 +1,39 @@
 class Public::PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post =  Post.new(post_params)
+    if post.save
+      flash[:notice] = "投稿成功！(o^―^o)"
+      redirect_to posts_path
+    else
+      flash[:alert] = "投稿に失敗しました(´・ω・`)"
+      render :new
+    end
+  end
+
   def index
+  end
+
+  def show 
+  end
+
+  def edit 
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :images [])
   end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -34,7 +34,7 @@ class Public::UsersController < ApplicationController
 
   private 
   def user_params
-    params.require[:user].permit[:name, :profile_image, :introduction]
+    params.requir(:user).permit(:name, :profile_image, :introduction)
   end
 
   def ensure_current_user

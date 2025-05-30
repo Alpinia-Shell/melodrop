29ec99f0f91a3e5eb77b3876ef56d39c525ea83f
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-05-30 07:45:34 +0000

    Fix:バリデーション修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-  validates :title,  length: { minimum: 2 , maximum: 20 }
-  validates :body, length: { minimum: 1, maximum: 200 }
+  validates :title,  length: { minimum: 2 , maximum: 50 }
+  validates :body, length: { minimum: 1, maximum: 2200 }
 end

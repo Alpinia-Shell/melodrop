30b91db91303ab1582c3979aacf10d95c1ab1793
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-06-03 06:46:33 +0000

    Add:ステータス変更機能追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,4 +7,20 @@ class Admin::UsersController < Admin::BaseController
     @user = User.find(params[:id])
     @posts = @user.posts
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "ステータスの変更が完了しました"
+    redirect_to admin_user_path(@user)
+    else
+      flash[:aleart] = "ステータスの変更に失敗しました"
+      render :show
+    end
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:is_active)
+  end
 end

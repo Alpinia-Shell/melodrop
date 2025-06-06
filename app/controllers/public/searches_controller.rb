bd97330f0a869bfb5871c62dde6cd82484b0bfcb
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-06-06 04:15:30 +0000

    Add:検索結果画面レスポンシブ

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,7 +2,7 @@ class Public::SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
-    @posts = Post.where("title LIKE ? OR body LIKE ?", "%#{params[:content]}%", "%#{params[:content]}%")
+    @posts = Post.where("title LIKE ? OR body LIKE ?", "%#{@content}%", "%#{@content}%")
     @content = params[:content]
   end
 end

63456f164a7d6f69314b5bbb21df527ca6290d33
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-06-07 06:15:38 +0000

    Add:マイページいいね一覧表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorited_posts, through: :favorites, source: :post
 
   GUEST_USER_EMAIL = "guest@example.com"
 

bc629c7642da344dd3b90afb04b5ffb4579a75f4
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-06-07 03:45:58 +0000

    Add:アソシエーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ApplicationRecord
   validates :title,  length: { minimum: 2 , maximum: 50 }
   validates :body, length: { minimum: 1, maximum: 2200 }
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.exists?(user_id.user.id)
+  end
 end

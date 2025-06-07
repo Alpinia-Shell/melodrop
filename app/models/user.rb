e84a8d54614351b9d10c0fbced148c8e0ac6877b
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-06-07 07:26:50 +0000

    Add:ビューの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_posts, through: :favorites, source: :post
-  has_many :active_relationships,class_name: "Relationship", foreign_key: "follower_id", depenfent: :destroy
+  has_many :active_relationships,class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
@@ -20,13 +20,13 @@ class User < ApplicationRecord
   end
 
   def unfollow(user)
-    active_relationships.find_by(followed_id: user_id).destroy
+    active_relationships.find_by(followed_id: user.id).destroy
   end
 
   def following?(user)
     followings.include?(user)
   end
-  
+
   GUEST_USER_EMAIL = "guest@example.com"
 
   def get_profile_image(width, height)

b38b105006774ad068b2dd744f6e9bf258c108ae
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-06-07 04:31:52 +0000

    Add:view追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,6 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   def favorited_by?(user)
-    favorites.exists?(user_id.user.id)
+    favorites.exists?(user_id: user.id)
   end
 end

ef02f82522df84e323f978658e76c2e15b894f58
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-06-07 09:23:14 +0000

    Add:フォローフォロワー一覧

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -4,18 +4,18 @@ class Public::RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     current_user.follow(@user)
-    redirect_to user_path(@user)
+    redirect_to request.referer
   end
 
   def destroy
     @user = User.find(params[:user_id])
     current_user.unfollow(@user)
-    redirect_to user_path(@user)
+    redirect_to request.referer
   end
 
   def followings
     user = User.find(params[:user_id])
-    @users = user.followigns
+    @users = user.followings
   end
 
   def followers

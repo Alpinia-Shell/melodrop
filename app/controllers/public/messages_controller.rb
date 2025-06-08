1aaf61f83b8d4b495bd4946b0739d769b1f2a955
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-06-08 06:02:04 +0000

    Add:dm機能追加

--- app/controllers/public/messages_controller.rb
+++ app/controllers/public/messages_controller.rb
@@ -1,2 +1,23 @@
 class Public::MessagesController < ApplicationController
+  before_action :authenticate_user!, only:[:create]
+
+  def create
+    if Entry.exists?(user_id: current_user.id, room_id: params[:message][:room_id])
+      @message = Message.new(message_params)
+      @message.user_id = current_user.id
+      if @message.save
+        redirect_to request.referer
+      else
+        flash[:alert] = "メッセージの送信に失敗しました"
+        redirect_back(fallback_location: root_path)
+      end
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+  def message_params 
+    params.require(:message).permit(:message, :room_id)
+  end
 end

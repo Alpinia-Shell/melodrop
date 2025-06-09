88fb0b9d8fe7cb31e893bf0b5adb09014c8ea972
Alpinia-Shell <201705012+Alpinia-Shell@users.noreply.github.com>
2025-06-09 07:32:41 +0000

    Add:DMデザイン修正

--- app/helpers/public/notifications_helper.rb
+++ app/helpers/public/notifications_helper.rb
@@ -2,7 +2,7 @@ module Public::NotificationsHelper
   def notification_message(notification)
     case notification.notifiable_type
     when "Post"
-      "見守っている#{notification.notifiable.user.name}さんが#{notification.notifiable.title}の雫を落としました"
+      "見守っている#{notification.notifiable.user.name}さんが「#{notification.notifiable.title}」」の雫を落としました"
     else
       "落とした「#{notification.notifiable.post.title}」の雫に#{notification.notifiable.user.name}さんがカケラを残してくれました"
     end
